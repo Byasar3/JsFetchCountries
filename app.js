c8b08eaba9116554d72b02ff20e937b24b30e7df
Berna Yasar <bernayasar@hotmail.co.uk>
2023-01-18 18:12:59 +0000

    finally got name of country to show

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ console.log("HI!");
 const getCountryByName = async (countryName) => {
     // the initial fetch to the website, save as variable 'response'
     const response = await fetch (`https://restcountries.com/v3.1/name/${countryName}`);
-    console.log(response);
+    // console.log(response);
     // the promise made to get the json and save it to variable jsonData
     const jsonData = await response.json();
-    console.log(jsonData);
+    // console.log(jsonData);
 
+    // making a container, which will be kept in a div
+    // container will contain the name info of the country
+    const nameContainer = document.createElement("div"); 
+    
+    jsonData.forEach(country => {
+    const nameContent = document.createElement("p");
+    nameContent.textContent = country.name.common;
+    console.log(nameContent);
+
+    nameContainer.appendChild(nameContent);
+    
+    console.log(nameContainer);
+    document.querySelector("body").appendChild(nameContainer);
+    });
 
 
 }
@@ -16,9 +30,7 @@ getCountryByName("turkey")
 
 
 
-    // making a container, which will be kept in a div
-    // container will contain the information of country in an array
-    // const infoContainer = document.createElement("div");
+
 
 
     // // going to make a loop thats goes through the array and adds each element into the container
@@ -29,4 +41,4 @@ getCountryByName("turkey")
     //     infoContainer.appendChild(information);
     // })
     
-    // document.querySelector("body").appendChild(infoContainer);
+    

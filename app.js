4f7a3cd3a1e39c9863184c78f12757f547fe20f3
Berna Yasar <bernayasar@hotmail.co.uk>
2023-01-18 17:35:33 +0000

    lots of edits only to comment out code and try again

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 console.log("HI!");
 
-const getCountryByName = async () => {
-    const response = await fetch (`https://restcountries.com/v3.1/name/peru`);
-    // console.log(response);
+const getCountryByName = async (countryName) => {
+    // the initial fetch to the website, save as variable 'response'
+    const response = await fetch (`https://restcountries.com/v3.1/name/${countryName}`);
+    console.log(response);
+    // the promise made to get the json and save it to variable jsonData
     const jsonData = await response.json();
     console.log(jsonData);
-    // const info = document.querySelector('div');
-    // console.log(jsonData.message); 
-    
+
+
+
 }
 
-const button = document.querySelector("button");
+getCountryByName("turkey")
 
-button.addEventListener("click", getCountryByName);
+
+
+    // making a container, which will be kept in a div
+    // container will contain the information of country in an array
+    // const infoContainer = document.createElement("div");
+
+
+    // // going to make a loop thats goes through the array and adds each element into the container
+    // jsonData.message.forEach((informationUrl) => {
+    //     const information = document.createElement("li");
+    //     console.log(information);
+    //     information = informationUrl;
+    //     infoContainer.appendChild(information);
+    // })
+    
+    // document.querySelector("body").appendChild(infoContainer);
